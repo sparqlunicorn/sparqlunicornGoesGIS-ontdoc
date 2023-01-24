8cecafba33dd30e7a2adf9c9b3607a566688faa2
Timo <situx@users.noreply.github.com>
2023-01-24 21:08:06 +0100

    Update docgeneration.py

--- docgeneration.py
+++ docgeneration.py
@@ -2249,7 +2249,7 @@ if not os.path.exists(outpath+'/index.html'):
         .replace("{{classtreefolderpath}}",prefixnsshort + "_classtree.js").replace("{{baseurlhtml}}", ".").replace("{{proprelationpath}}", "proprelations.js").replace("{{scriptfolderpath}}", prefixnsshort+ '_search.js').replace("{{exports}}",nongeoexports)
     indexhtml=indexhtml.replace("{{indexpage}}","true")	
     indexhtml+="<p>This page shows information about linked data resources in HTML. Choose the classtree navigation or search to browse the data</p>"
-    indexhtml+="<table class=\"description\" style =\"height: 100%; overflow: auto\" border=1 id=indextable><thead><tr><th>Dataset</th></tr></thead><tbody>"
+    indexhtml+="<table class=\"description\" border=1 id=indextable><thead><tr><th>Dataset</th></tr></thead><tbody>"
     subfolders= [f.path for f in os.scandir(outpath) if f.is_dir()]
     print(subfolders)
     for path in subfolders:
