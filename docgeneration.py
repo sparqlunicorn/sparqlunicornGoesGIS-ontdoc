0e19dc5493b1192f00f9516344698b05bed8fc17
Timo <situx@users.noreply.github.com>
2023-03-29 14:37:32 +0200

    add vowl to template

--- docgeneration.py
+++ docgeneration.py
@@ -1342,7 +1342,7 @@ baseMaps["OSM"].addTo(map);
 </script>
 """
 
-
+vowltemplate=""
 
 htmlcommenttemplate="""<p class="comment"><b>Description:</b> {{comment}}</p>"""
 
@@ -1410,6 +1410,10 @@ def resolveTemplate(templatename):
             with open(templatepath+"/"+templatename+"/templates/3dtemplate.html", 'r') as file:
                 global image3dtemplate
                 image3dtemplate=file.read()
+        if os.path.exists(templatepath+"/"+templatename+"/templates/vowlwrapper.html"):
+            with open(templatepath+"/"+templatename+"/templates/vowlwrapper.html", 'r') as file:
+                global vowltemplate
+                vowltemplate=file.read()
         if os.path.exists(templatepath+"/"+templatename+"/templates/audiotemplate.html"):
             with open(templatepath+"/"+templatename+"/templates/audiotemplate.html", 'r') as file:
                 global audiotemplate
@@ -1699,7 +1703,7 @@ class OntDocGeneration:
                     indexhtml=indexhtml.replace("{{indexpage}}","true")
                 else:
                     indexhtml = indexhtml.replace("{{indexpage}}", "false")
-                indexhtml+="<p>This page shows information about linked data resources in HTML. Choose the classtree navigation or search to browse the data</p>"
+                indexhtml+="<p>This page shows information about linked data resources in HTML. Choose the classtree navigation or search to browse the data</p>"+vowltemplate.replace("{{vowlpath}}", "minivowl_result.js")
                 indexhtml+="<table class=\"description\" style =\"height: 100%; overflow: auto\" border=1 id=indextable><thead><tr><th>Class</th><th>Number of instances</th><th>Instance Example</th></tr></thead><tbody>"
                 for item in tree["core"]["data"]:
                     if (item["type"]=="geoclass" or item["type"]=="class" or item["type"]=="featurecollection" or item["type"]=="geocollection") and "instancecount" in item and item["instancecount"]>0:
