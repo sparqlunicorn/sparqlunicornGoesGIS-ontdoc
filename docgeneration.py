33d196b2269357fc3f565eff91b052aee03d5e9e
situx <timo.homburg@gmx.de>
2023-05-08 02:40:31 +0200

    fixes

--- docgeneration.py
+++ docgeneration.py
@@ -2426,7 +2426,7 @@ class OntDocGeneration:
                 elif str(tup)==self.typeproperty:
                     for tp in predobjmap[tup]:
                         if str(tp) in bibtextypemappings:
-                            itembibtex=self.resolveBibtexReference(graph.predicate_objects(subject),subject)
+                            itembibtex=self.resolveBibtexReference(graph.predicate_objects(subject),subject,graph)
                             break
                 thetable=self.formatPredicate(tup, baseurl, checkdepth, thetable, graph,inverse)
                 if str(tup) in labelproperties:
