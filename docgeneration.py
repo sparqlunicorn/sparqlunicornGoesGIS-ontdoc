e0bcb3cdee75d07e8eb74e96082eb832aac53042
Timo <situx@users.noreply.github.com>
2023-05-16 18:03:58 +0200

    fixes

--- docgeneration.py
+++ docgeneration.py
@@ -2385,17 +2385,17 @@ class OntDocGeneration:
             os.mkdir(outpath + "/conformance/")
         for coll in featurecollectionspaths:
             collectionsjson["collections"].append({"id":featurecollectionspaths[coll]["id"],"title":featurecollectionspaths[coll]["name"],"links":[{"href":str(self.deploypath)+"/collections/"+str(featurecollectionspaths[coll]["id"]),"rel":"collection","type":"application/json","title":"Collection as JSON"},{"href":str(self.deploypath)+"/collections/"+str(featurecollectionspaths[coll]["id"]),"rel":"collection","type":"text/html","title":"Collection as HTML"}]})
-            if not os.path.exists(outpath+"/collections/"+coll.replace(outpath,"")+"/"):
+            if not os.path.exists(outpath+"/collections/"+coll.replace(outpath,"").replace("index.geojson","")+"/"):
                 os.mkdir(outpath+"/collections/"+coll.replace(outpath,"").replace("index.geojson","")+"/")
             currentcollection={"title":featurecollectionspaths[coll]["name"],"id":featurecollectionspaths[coll]["id"],"links":[]}
             currentcollection["links"]=[{"href":str(self.deploypath)+"/"+coll.replace(outpath,""),"rel":"items","type":"application/json","title":"Collection as JSON"},{"href":str(self.deploypath)+"/"+coll.replace(outpath,"").replace(".geojson",".html"),"rel":"items","type":"text/html","title":"Collection as HTML"}]
-            f=open(outpath+"/collections/"+coll.replace(outpath,""),"w",encoding="utf-8")
+            f=open(outpath+"/collections/"+coll.replace(outpath,"").replace("index.geojson","")+"/index.json","w",encoding="utf-8")
             f.write(json.dumps(currentcollection))
             f.close()  
         f=open(outpath + "/index.json","w",encoding="utf-8")
         f.write(json.dumps(landingpagejson))
         f.close()
-        f=open(outpath + "api/index.json","w",encoding="utf-8")
+        f=open(outpath + "/api/index.json","w",encoding="utf-8")
         f.write(json.dumps(apipagejson))
         f.close()         
         f=open(outpath + "/collections/index.json","w",encoding="utf-8")
