0dd08431d5f35834a0b27a5fce21be013303f4ec
Timo Homburg <timo.homburg@hs-mainz.de>
2023-05-17 02:44:22 +0200

    update

--- docgeneration.py
+++ docgeneration.py
@@ -2391,9 +2391,9 @@ class OntDocGeneration:
             if not os.path.exists(op):
                 os.mkdir(op)
             opweb=op.replace(outpath,self.deploypath)
-            collectionsjson["collections"].append({"id":coll.replace(outpath,"").replace("index.geojson","")[1:],"title":featurecollectionspaths[coll]["name"],"links":[{"href":opweb,"rel":"collection","type":"application/json","title":"Collection as JSON"},{"href":opweb,"rel":"collection","type":"text/html","title":"Collection as HTML"}]})
+            collectionsjson["collections"].append({"id":coll.replace(outpath,"").replace("index.geojson","")[1:],"title":featurecollectionspaths[coll]["name"],"links":[{"href":opweb.replace(".geojson",""),"rel":"collection","type":"application/json","title":"Collection as JSON"},{"href":opweb.replace(".geojson",""),"rel":"collection","type":"text/html","title":"Collection as HTML"}]})
             currentcollection={"title":featurecollectionspaths[coll]["name"],"id":coll.replace(outpath,"").replace("index.geojson","").replace(".geojson","")[1:],"links":[]}
-            currentcollection["links"]=[{"href":opweb,"rel":"items","type":"application/json","title":"Collection as JSON"},{"href":opweb,"rel":"items","type":"text/html","title":"Collection as HTML"}]
+            currentcollection["links"]=[{"href":opweb.replace(".geojson",""),"rel":"items","type":"application/json","title":"Collection as JSON"},{"href":opweb.replace(".geojson",""),"rel":"items","type":"text/html","title":"Collection as HTML"}]
             f=open(op+"index.json","w",encoding="utf-8")
             f.write(json.dumps(currentcollection))
             f.close()  
