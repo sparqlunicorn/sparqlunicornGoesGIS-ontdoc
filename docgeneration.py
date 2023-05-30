f11821c905786be99a7f10dac5f01959f7c96c88
situx <timo.homburg@gmx.de>
2023-05-30 02:12:48 +0200

    update

--- docgeneration.py
+++ docgeneration.py
@@ -2368,9 +2368,9 @@ class OntDocGeneration:
             curiiifmanifest={"@context": "http://iiif.io/api/presentation/3/context.json","id":imgpath+"/manifest.json", "type": "Manifest","label":{"en":[str(label)+" ("+self.shortenURI(imgpath)+")"]},"items":[{"id":imgpath+"/canvas/p1","type":"Canvas","height":100,"width":100,"items":[{"id":imgpath+"/canvas/p1/1","type":"AnnotationPage","items":[{"id":imgpath+"/annotation/p1/1","type":"Annotation","motivation":"painting","body":{"id":imgpath,"type":"Image","format":"image/png"},"target":imgpath+"/canvas/p1"}]}],"annotations":[{"id":imgpath+"/canvas/p1/annopage-2","type":"AnnotationPage","items":[{"id":imgpath+"/canvas/p1/anno-1","type":"Annotation","motivation":"commenting","body":{"type":"TextualBody","language":"en","format":"text/html","value":"<a href=\""+str(curind)+"\">"+str(label)+"</a>"},"target":imgpath+"/canvas/p1"}]}]}]}
         else:
             curiiifmanifest={"@context": "http://iiif.io/api/presentation/3/context.json","id":imgpath+"/manifest.json", "type": "Manifest","label":{"en":[self.shortenURI(imgpath)]},"items":[{"id":imgpath+"/canvas/p1","type":"Canvas","height":100,"width":100,"items":[{"id":imgpath+"/canvas/p1/1","type":"AnnotationPage","items":[{"id":imgpath+"/annotation/p1/1","type":"Annotation","motivation":"painting","body":{"id":imgpath,"type":"Image","format":"image/png"},"target":imgpath+"/canvas/p1"}]}],"annotations":[{"id":imgpath+"/canvas/p1/annopage-2","type":"AnnotationPage","items":[{"id":imgpath+"/canvas/p1/anno-1","type":"Annotation","motivation":"commenting","body":{"type":"TextualBody","language":"en","format":"text/html","value":"<a href=\""+str(curind)+"\">"+str(self.shortenURI(curind))+"</a>"},"target":imgpath+"/canvas/p1"}]}]}]}
-        os.makedirs(self.outpath + "/iiif/images/"+self.shortenURI(imgpath)+"/full/")
-        os.makedirs(self.outpath + "/iiif/images/"+self.shortenURI(imgpath)+"/full/full/")
-        os.makedirs(self.outpath + "/iiif/images/"+self.shortenURI(imgpath)+"/full/full/0/")
+        #os.makedirs(self.outpath + "/iiif/images/"+self.shortenURI(imgpath)+"/full/")
+        #os.makedirs(self.outpath + "/iiif/images/"+self.shortenURI(imgpath)+"/full/full/")
+        #os.makedirs(self.outpath + "/iiif/images/"+self.shortenURI(imgpath)+"/full/full/0/")
         os.makedirs(self.outpath + "/iiif/mf/"+self.shortenURI(imgpath))
         f=open(self.outpath+"/iiif/mf/"+self.shortenURI(imgpath)+"/manifest.json","w",encoding="utf-8")
         f.write(json.dumps(curiiifmanifest))
