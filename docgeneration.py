3199e1a0dea81592c366d95b9472bec92d99b1c0
Timo Homburg <timo.homburg@hs-mainz.de>
2023-05-31 01:00:23 +0200

    update

--- docgeneration.py
+++ docgeneration.py
@@ -2376,11 +2376,10 @@ class OntDocGeneration:
                 pagecounter+=1
             for pred in predobjmap:
                 for objs in predobjmap[pred]:
-                    if not str(objs) in imgpaths:
-                        if isinstance(objs,URIRef):
-                            curiiifmanifest["metadata"].append({"label":{"en":[self.shortenURI(pred)]},"value":{"en":["<a href=\""+str(objs)+"\">"+str(objs)+"</a>"]}})
-                        else:
-                            curiiifmanifest["metadata"].append({"label":{"en":[self.shortenURI(pred)]},"value":{"en":[str(objs)]}})
+                    if isinstance(objs,URIRef):
+                        curiiifmanifest["metadata"].append({"label":{"en":[self.shortenURI(pred)]},"value":{"en":["<a href=\""+str(objs)+"\">"+str(objs)+"</a>"]}})
+                    else:
+                        curiiifmanifest["metadata"].append({"label":{"en":[self.shortenURI(pred)]},"value":{"en":[str(objs)]}})
             if summary!=None and summary!="" and summary!={}:
                 curiiifmanifest["summary"]={"en":[str(summary)]}
             #os.makedirs(self.outpath + "/iiif/images/"+self.shortenURI(imgpath)+"/full/")
