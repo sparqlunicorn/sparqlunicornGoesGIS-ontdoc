61093a26a1d8906229905141a5554188aa2070c4
situx <timo.homburg@gmx.de>
2023-06-05 02:46:07 +0200

    anno test

--- docgeneration.py
+++ docgeneration.py
@@ -2940,7 +2940,7 @@ class OntDocGeneration:
                     if self.iiif:
                         iiifmanifestpaths["default"].append(self.generateIIIFManifest(outpath,foundmedia["mesh"],image3dannos,str(subject),self.prefixnamespace,foundlabel,comment,thetypes,predobjmap,"Model"))
                     for anno in image3dannos:
-                        if ("POINT" in anno.upper() or "POLYGON" in anno.upper() or "LINESTRING" in anno.upper()):
+                        if ("POINT" in anno["value"].upper() or "POLYGON" in anno["value"].upper() or "LINESTRING" in anno["value"].upper()):
                             f.write(threejstemplate.replace("{{wktstring}}",anno["value"]).replace("{{meshurls}}",str(list(foundmedia["mesh"]))))
                 elif len(foundmedia["mesh"])>0 and len(image3dannos)==0:
                     print("Found 3D Model: "+str(foundmedia["mesh"]))
