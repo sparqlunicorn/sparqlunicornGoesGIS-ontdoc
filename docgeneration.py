ba5fc6b4e835387dd12d3a73cfc7affe12c4bc6b
Timo <situx@users.noreply.github.com>
2023-10-07 22:05:26 +0200

    merge

--- docgeneration.py
+++ docgeneration.py
@@ -1433,6 +1433,89 @@ def resolveTemplate(templatename):
         return True
     return False
 
+class Exporter:
+    
+    @staticmethod
+    def serializeRDF(g,file,subjectstorender,formatt):
+        g.serialize(file,encoding="utf-8",format=formatt)
+
+    @staticmethod
+    def convertTTLToCypher(g,file,subjectstorender=None,formatt="cypher"):
+        uriToNodeId={}
+        nodecounter=0
+        tgfresedges=""
+        if subjectstorender==None:
+            subjectstorender=g.subjects()
+        for sub in subjectstorender:
+            uriToNodeId[str(sub)]=nodecounter
+            file.write("CREATE ( "+str(self.shortenURI(str(sub)))+"{ _id:'"+str(self.shortenURI(str(sub)))+"', _uri:'"+str(sub)+"', rdfs_label:'"+str(self.shortenURI(str(sub)))+"' })\n")
+            nodecounter+=1
+            for tup in g.predicate_objects(sub):
+                if str(tup[1]) not in uriToNodeId:
+                    file.write("CREATE ( "+str(self.shortenURI(str(tup[1])))+"{ _id:'"+str(self.shortenURI(str(tup[1])))+"', _uri:'"+str(tup[1])+"', rdfs_label:'"+str(self.shortenURI(str(tup[1])))+"' })\n")
+                    uriToNodeId[str(tup[1])]=nodecounter
+                    nodecounter+=1
+                tgfresedges+="("+str(uriToNodeId[str(sub)])+")-[:"+str(self.shortenURI(str(tup[1])))+"]->("+str(self.shortenURI(tup[0]))+"),\n"
+        file.write("\n\nCREATE ")
+        file.write(tgfresedges[0:-2]+"\n")
+        return None        
+
+    @staticmethod
+    def convertTTLToGraphML(g,file,subjectstorender=None,formatt="graphml"):
+        literalcounter=0
+        edgecounter=0
+        file.write("""<?xml version="1.0" encoding="UTF-8"?>
+<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:y="http://www.yworks.com/xml/graphml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
+<key for="node" id="nodekey" yfiles.type="nodegraphics"></key><key for="edge" id="edgekey" yfiles.type="edgegraphics"></key><graph id="G" edgedefault="directed">""")
+        if subjectstorender==None:
+            subjectstorender=g.subjects()
+        addednodes=set()
+        for sub in subjectstorender:
+            file.write("<node id=\""+str(sub)+"\" uri=\""+str(sub)+"\"><data key=\"nodekey\"><y:ShapeNode><y:Shape shape=\"ellipse\"></y:Shape><y:Fill color=\"#800080\" transparent=\"false\"></y:Fill><y:NodeLabel alignment=\"center\" fontSize=\"12\" fontStyle=\"plain\" hasText=\"true\" visible=\"true\" width=\"4.0\">"+str(self.shortenURI(sub))+"</y:NodeLabel></y:ShapeNode></data></node>\n")
+            for tup in g.predicate_objects(sub):
+                if isinstance(tup[1],Literal):
+                    file.write("<node id=\"literal"+str(literalcounter)+"\" uri=\"literal"+str(literalcounter)+"\"><data key=\"nodekey\"><y:ShapeNode><y:Shape shape=\"ellipse\"></y:Shape><y:Fill color=\"#008000\" transparent=\"false\"></y:Fill><y:NodeLabel alignment=\"center\" fontSize=\"12\" fontStyle=\"plain\" hasText=\"true\" visible=\"true\" width=\"4.0\"><![CDATA["+str(tup[1])+"]]></y:NodeLabel></y:ShapeNode></data></node>\n")
+                    file.write("<edge id=\"e"+str(edgecounter)+"\" uri=\""+str(tup[0])+"\" source=\""+str(sub)+"\" target=\"literal"+str(literalcounter)+"\"><data key=\"edgekey\"><y:PolyLineEdge><y:EdgeLabel alignment=\"center\" configuration=\"AutoFlippingLabel\" fontSize=\"12\" fontStyle=\"plain\" hasText=\"true\" visible=\"true\" width=\"4.0\">"+str(self.shortenURI(str(tup[0])))+"</y:EdgeLabel></y:PolyLineEdge></data></edge>\n")
+                    literalcounter+=1
+                else:
+                    if tup[1] not in subjectstorender and str(tup[1]) not in addednodes:
+                        file.write("<node id=\""+str(tup[1])+"\" uri=\""+str(tup[1])+"\"><data key=\"nodekey\"><y:ShapeNode><y:Shape shape=\"ellipse\"></y:Shape><y:Fill color=\"#800080\" transparent=\"false\"></y:Fill><y:NodeLabel alignment=\"center\" fontSize=\"12\" fontStyle=\"plain\" hasText=\"true\" visible=\"true\" width=\"4.0\">"+str(self.shortenURI(str(tup[1])))+"</y:NodeLabel></y:ShapeNode></data></node>\n")
+                        addednodes.add(str(tup[1]))
+                    file.write("<edge id=\"e"+str(edgecounter)+"\" uri=\""+str(tup[0])+"\" source=\""+str(sub)+"\" target=\""+str(tup[1])+"\"><data key=\"edgekey\"><y:PolyLineEdge><y:EdgeLabel alignment=\"center\" configuration=\"AutoFlippingLabel\" fontSize=\"12\" fontStyle=\"plain\" hasText=\"true\" visible=\"true\" width=\"4.0\">"+str(self.shortenURI(str(tup[1])))+"</y:EdgeLabel></y:PolyLineEdge></data></edge>\n")
+                edgecounter+=1
+        file.write("</graph></graphml>")
+        return None
+
+    @staticmethod
+    def convertTTLToTGF(g,file,subjectstorender=None,formatt="tgf"):
+        uriToNodeId = {}
+        nodecounter = 0
+        tgfresedges = ""
+        sepchar=" "
+        if subjectstorender == None:
+            subjectstorender = g.subjects(None,None,True)
+        if formatt=="GDF":
+            sepchar=","
+            file.write("nodedef>name VARCHAR,label VARCHAR")
+        for sub in subjectstorender:
+            if str(sub) not in uriToNodeId:
+                uriToNodeId[str(sub)] = nodecounter
+                file.write(str(nodecounter) + sepchar + str(sub) + "\n")
+                nodecounter += 1
+            for tup in g.predicate_objects(sub):
+                if str(tup[1]) not in uriToNodeId:
+                    file.write(str(nodecounter) + sepchar + str(tup[1]) + "\n")
+                    uriToNodeId[str(tup[1])] = nodecounter
+                    nodecounter += 1
+                tgfresedges += str(uriToNodeId[str(sub)]) + sepchar + str(uriToNodeId[str(tup[1])]) + sepchar + str(
+                    GraphExporter.shortenURI(tup[0])) + "\n"
+        if formatt=="gdf":
+            file.write("edgedef>node1 VARCHAR,node2 VARCHAR,label VARCHAR\n")
+        else:
+            file.write("#\n")
+        file.write(tgfresedges)
+        return None
+
 class OntDocGeneration:
 
     def __init__(self, prefixes,prefixnamespace,prefixnsshort,license,labellang,outpath,graph,createIndexPages,createColl,metadatatable,generatePagesForNonNS,createVOWL,ogcapifeatures,iiif,localOptimized=False,startconcept=None,deploypath="",logoname="",templatename="default",offlinecompat=False,exports=["json","ttl"],datasettitle=""):
@@ -1454,7 +1537,7 @@ class OntDocGeneration:
         self.geocollectionspaths=[]
         self.metadatatable=metadatatable
         self.templatename=templatename
-        self.exportToFunction={"cypher":self.convertTTLToCypher,"graphml":self.convertTTLToGraphML,"tgf":self.convertTTLToTGF,"ttl":self.serializeRDF,"trig":self.serializeRDF,"xml":self.serializeRDF,"trix":self.serializeRDF,"nt":self.serializeRDF,"n3":self.serializeRDF,"nquads":self.serializeRDF}
+        self.exportToFunction={"cypher":Exporter.convertTTLToCypher,"graphml":Exporter.convertTTLToGraphML,"tgf":Exporter.convertTTLToTGF,"ttl":Exporter.serializeRDF,"trig":Exporter.serializeRDF,"xml":Exporter.serializeRDF,"trix":Exporter.serializeRDF,"nt":Exporter.serializeRDF,"n3":Exporter.serializeRDF,"nquads":Exporter.serializeRDF}
         resolveTemplate(templatename)
         self.offlinecompat=offlinecompat
         if offlinecompat:
@@ -1546,74 +1629,6 @@ class OntDocGeneration:
                 fd.write(r.content)
             myhtmltemplate=myhtmltemplate.replace(match,"{{relativepath}}css/"+match[match.rfind("/")+1:])        
         return myhtmltemplate
-
-    def serializeRDF(self,g,file,subjectstorender,formatt):
-        g.serialize(file,encoding="utf-8",format=formatt)
-
-    def convertTTLToCypher(self,g,file,subjectstorender=None,formatt="cypher"):
-        uriToNodeId={}
-        nodecounter=0
-        tgfresedges=""
-        if subjectstorender==None:
-            subjectstorender=g.subjects()
-        for sub in subjectstorender:
-            uriToNodeId[str(sub)]=nodecounter
-            file.write("CREATE ( "+str(self.shortenURI(str(sub)))+"{ _id:'"+str(self.shortenURI(str(sub)))+"', _uri:'"+str(sub)+"', rdfs_label:'"+str(self.shortenURI(str(sub)))+"' })\n")
-            nodecounter+=1
-            for tup in g.predicate_objects(sub):
-                if str(tup[1]) not in uriToNodeId:
-                    file.write("CREATE ( "+str(self.shortenURI(str(tup[1])))+"{ _id:'"+str(self.shortenURI(str(tup[1])))+"', _uri:'"+str(tup[1])+"', rdfs_label:'"+str(self.shortenURI(str(tup[1])))+"' })\n")
-                    uriToNodeId[str(tup[1])]=nodecounter
-                    nodecounter+=1
-                tgfresedges+="("+str(uriToNodeId[str(sub)])+")-[:"+str(self.shortenURI(str(tup[1])))+"]->("+str(self.shortenURI(tup[0]))+"),\n"
-        file.write("\n\nCREATE ")
-        file.write(tgfresedges[0:-2]+"\n")
-        return None        
-
-    def convertTTLToGraphML(self,g,file,subjectstorender=None,formatt="graphml"):
-        literalcounter=0
-        edgecounter=0
-        file.write("""<?xml version="1.0" encoding="UTF-8"?>
-<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:y="http://www.yworks.com/xml/graphml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
-<key for="node" id="nodekey" yfiles.type="nodegraphics"></key><key for="edge" id="edgekey" yfiles.type="edgegraphics"></key><graph id="G" edgedefault="directed">""")
-        if subjectstorender==None:
-            subjectstorender=g.subjects()
-        addednodes=set()
-        for sub in subjectstorender:
-            file.write("<node id=\""+str(sub)+"\" uri=\""+str(sub)+"\"><data key=\"nodekey\"><y:ShapeNode><y:Shape shape=\"ellipse\"></y:Shape><y:Fill color=\"#800080\" transparent=\"false\"></y:Fill><y:NodeLabel alignment=\"center\" fontSize=\"12\" fontStyle=\"plain\" hasText=\"true\" visible=\"true\" width=\"4.0\">"+str(self.shortenURI(sub))+"</y:NodeLabel></y:ShapeNode></data></node>\n")
-            for tup in g.predicate_objects(sub):
-                if isinstance(tup[1],Literal):
-                    file.write("<node id=\"literal"+str(literalcounter)+"\" uri=\"literal"+str(literalcounter)+"\"><data key=\"nodekey\"><y:ShapeNode><y:Shape shape=\"ellipse\"></y:Shape><y:Fill color=\"#008000\" transparent=\"false\"></y:Fill><y:NodeLabel alignment=\"center\" fontSize=\"12\" fontStyle=\"plain\" hasText=\"true\" visible=\"true\" width=\"4.0\"><![CDATA["+str(tup[1])+"]]></y:NodeLabel></y:ShapeNode></data></node>\n")
-                    file.write("<edge id=\"e"+str(edgecounter)+"\" uri=\""+str(tup[0])+"\" source=\""+str(sub)+"\" target=\"literal"+str(literalcounter)+"\"><data key=\"edgekey\"><y:PolyLineEdge><y:EdgeLabel alignment=\"center\" configuration=\"AutoFlippingLabel\" fontSize=\"12\" fontStyle=\"plain\" hasText=\"true\" visible=\"true\" width=\"4.0\">"+str(self.shortenURI(str(tup[0])))+"</y:EdgeLabel></y:PolyLineEdge></data></edge>\n")
-                    literalcounter+=1
-                else:
-                    if tup[1] not in subjectstorender and str(tup[1]) not in addednodes:
-                        file.write("<node id=\""+str(tup[1])+"\" uri=\""+str(tup[1])+"\"><data key=\"nodekey\"><y:ShapeNode><y:Shape shape=\"ellipse\"></y:Shape><y:Fill color=\"#800080\" transparent=\"false\"></y:Fill><y:NodeLabel alignment=\"center\" fontSize=\"12\" fontStyle=\"plain\" hasText=\"true\" visible=\"true\" width=\"4.0\">"+str(self.shortenURI(str(tup[1])))+"</y:NodeLabel></y:ShapeNode></data></node>\n")
-                        addednodes.add(str(tup[1]))
-                    file.write("<edge id=\"e"+str(edgecounter)+"\" uri=\""+str(tup[0])+"\" source=\""+str(sub)+"\" target=\""+str(tup[1])+"\"><data key=\"edgekey\"><y:PolyLineEdge><y:EdgeLabel alignment=\"center\" configuration=\"AutoFlippingLabel\" fontSize=\"12\" fontStyle=\"plain\" hasText=\"true\" visible=\"true\" width=\"4.0\">"+str(self.shortenURI(str(tup[1])))+"</y:EdgeLabel></y:PolyLineEdge></data></edge>\n")
-                edgecounter+=1
-        file.write("</graph></graphml>")
-        return None
-
-    def convertTTLToTGF(self,g,file,subjectstorender=None,formatt="tgf"):
-        uriToNodeId={}
-        nodecounter=0
-        tgfresedges=""
-        if subjectstorender==None:
-            subjectstorender=g.subjects()
-        for sub in subjectstorender:
-            uriToNodeId[str(sub)]=nodecounter
-            file.write(str(nodecounter)+" "+str(sub)+"\n")
-            nodecounter+=1
-            for tup in g.predicate_objects(sub):
-                if str(tup[1]) not in uriToNodeId:
-                    file.write(str(nodecounter)+" "+str(tup[1])+"\n")
-                    uriToNodeId[str(tup[1])]=nodecounter
-                    nodecounter+=1
-                tgfresedges+=str(uriToNodeId[str(sub)])+" "+str(uriToNodeId[str(tup[1])])+" "+str(self.shortenURI(tup[0]))+"\n"
-        file.write("#\n")
-        file.write(tgfresedges)
-        return None
 
     def convertOWL2MiniVOWL(self,g,outpath,predicates=[],typeproperty="http://www.w3.org/1999/02/22-rdf-syntax-ns#type",labelproperty="http://www.w3.org/2000/01/rdf-schema#label"):
         minivowlresult={"info": [{
