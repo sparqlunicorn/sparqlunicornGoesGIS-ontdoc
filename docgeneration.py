13074a8befb61547a55fe8eca5c69407e813ae53
Timo Homburg <timo.homburg@hs-mainz.de>
2023-10-26 00:52:33 +0200

    update

--- docgeneration.py
+++ docgeneration.py
@@ -6,8 +6,12 @@ from urllib.request import urlopen
 import urllib.parse
 from io import BytesIO
 from zipfile import ZipFile
-from pathlib import Path
-from PIL import Image
+from .doc.docutils import DocUtils
+from .export.data.graphexporter import GraphExporter
+from .export.data.miscexporter import MiscExporter
+from .export.api.iiifexporter import IIIFAPIExporter
+from .export.api.ogcapifeaturesexporter import OGCAPIFeaturesExporter
+from .export.api.ckanexporter import CKANExporter
 import requests
 import shapely.wkt
 import shapely.geometry
@@ -17,13 +21,7 @@ import re
 import shutil
 import json
 import sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from .doc.docutils import DocUtils
-from .export.data.graphexporter import GraphExporter
-from .export.data.miscexporter import MiscExporter
-from .export.api.iiifexporter import IIIFAPIExporter
-from .export.api.ogcapifeaturesexporter import OGCAPIFeaturesExporter
-from .export.api.ckanexporter import CKANExporter
+
 
 
 
