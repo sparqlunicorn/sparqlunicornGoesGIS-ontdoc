3652aeab654d10806b2471395588ef4cb586b461
Timo Homburg <timo.homburg@hs-mainz.de>
2023-11-07 00:57:35 +0100

    update

--- export/api/ckanexporter.py
+++ export/api/ckanexporter.py
@@ -26,7 +26,7 @@ class CKANExporter:
             os.makedirs(outpath + "/api/"+str(version)+"/action/tag_list/")
         p = Path(str(outpath + "/api/"+str(version)+"/action/group_list?all_fields=true"))
         p.symlink_to("./group_list/")
-        p = Path(str(outpath + "/api/"+str(version)+"/action/package_search?q=&sort=name%20asc&rows=50&start=0"))
+        p = Path(str(outpath + "/api/"+str(version)+"/action/package_search?q=&sort=name asc&rows=50&start=0"))
         p.symlink_to("./package_list/")
         f = open(outpath + "/api/"+str(version)+"/action/group_list/index.json", "w")
         f.write(json.dumps({"success": True, "result": []}))
