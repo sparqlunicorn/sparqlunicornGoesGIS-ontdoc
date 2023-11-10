8d504f45651d0f16602cadf9fb363d025dccab52
Timo Homburg <timo.homburg@hs-mainz.de>
2023-11-10 17:54:01 +0100

    update

--- export/api/solidexporter.py
+++ export/api/solidexporter.py
@@ -9,3 +9,12 @@ class SolidExporter:
         graph.add((URIRef(str(deploypath) + str(datasetname)),URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),URIRef("http://www.w3.org/ns/pim/space#Storage")))
         graph.add((URIRef(str(deploypath) + str(datasetname)), URIRef("http://www.w3.org/2000/01/rdf-schema#label"),
                    Literal("Solid Storage for "+str(datasetname))))
+
+    @staticmethod
+    def addSolidContainer(graph,deploypath,datasetname,collections):
+        for coll in collections:
+            graph.add((URIRef(coll),URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),URIRef("http://www.w3.org/ns/iana/media-types/text/ttl#Resource")))
+            graph.add((URIRef(coll), URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
+                       URIRef("http://www.w3.org/ns/ldp#Container")))
+            graph.add((URIRef(coll), URIRef("http://www.w3.org/ns/pim/space#storage"),
+                       URIRef(str(deploypath) + str(datasetname))))
