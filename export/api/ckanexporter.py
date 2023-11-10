2a8bf3af1963598431d17a40651ea00d88721d56
Timo <situx@users.noreply.github.com>
2023-11-10 14:17:45 +0100

    update

--- export/api/ckanexporter.py
+++ export/api/ckanexporter.py
@@ -78,8 +78,8 @@ class CKANExporter:
             if op.endswith("/"):
                 op = op[0:-1]
             if not os.path.exists(op):
-                os.makedirs(op
-            dataset={"success":True,"type":"dataset","title":curcollname,"result":{"id":curcollname,"license_id":license,"license_title":license,"name":curcollname,"notes":"","tags":[],"groups":[],"resources":[{"format":"text/ttl","url":deploypath+"/dataset/"+curcollname+".ttl"},{"format":"application/json","url":deploypath+"/dataset/"+curcollname+".json"},{"format":"text/html","url":deploypath+"/dataset/"+curcollname+".html"}]}}
+                os.makedirs(op)
+            dataset={"success":True,"type":"dataset","title":curcollname,"result":{"id":curcollname,"license_id":license,"license_title":license,"name":curcollname,"notes":"","tags":[],"groups":[],"resources":[{"format":"text/ttl","resource_type":"file","url":deploypath+"/dataset/"+curcollname+".ttl","state":"active","url_type":""},{"format":"application/json","resource_type":"file","url":deploypath+"/dataset/"+curcollname+".json","state":"active","url_type":""},{"format":"text/html","resource_type":"file","url":deploypath+"/dataset/"+curcollname+".html","state":"active","url_type":""}]}}
             f = open(outpath + "/dataset/"+curcollname+"_", "w")
             f.write(json.dumps(dataset))
             f.close()
