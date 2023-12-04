1028fa7b93d93ca2cfcd4451434ef67e09f46779
Timo <situx@users.noreply.github.com>
2023-12-04 13:11:58 +0100

    solid fixes

--- docgeneration.py
+++ docgeneration.py
@@ -3416,8 +3416,8 @@ class OntDocGeneration:
         if self.licenseuri!=None:
              ttlf.add((subject, URIRef("http://purl.org/dc/elements/1.1/license"), URIRef(self.licenseuri)))
         if self.solidexport!=None:
-            ttlf.add((subject, URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#"), URIRef("http://www.w3.org/ns/ldp#Resource")))
-            ttlf.add((subject, URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#"), URIRef("https://www.iana.org/assignments/media-types/text/turtle#Resource")))
+            ttlf.add((subject, URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), URIRef("http://www.w3.org/ns/ldp#Resource")))
+            ttlf.add((subject, URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), URIRef("https://www.iana.org/assignments/media-types/text/turtle#Resource")))
         nonnslink=""
         if nonns:
             completesavepath = savepath.replace(":","_")
