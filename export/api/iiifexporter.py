a027dcb2dd5883488a2605c0552a9fe9ea3b16c1
Timo <situx@users.noreply.github.com>
2023-12-11 01:40:54 +0100

    fixes

--- export/api/iiifexporter.py
+++ export/api/iiifexporter.py
@@ -159,7 +159,7 @@ class IIIFAPIExporter:
             categories.add(DocUtils.shortenURI(imgpath["class"]))
             for imgp in imgpath["imgpath"]:
                 imghtml+="<li data-groups='[\"all\",\"red\",\""+str(imgpath["class"])+"\"]' style=\"width:25%;background-color:white;border-radius:25px;\"><figure class=\"col-3@sm picture-item\"><div class=\"aspect aspect--16x9\"><div class=\"aspect__inner\">"
-                imghtml+="<a href=\""+str(deploypath)+"\"><img src=\""+str(imgp)+"\" loading=\"lazy\" class=\"imgborder\" alt=\""+str(imgpath["label"])+"\"/></a></div></div>"
+                imghtml+="<a href=\""+str(deploypath)+"/"+DocUtils.shortenURI(imgpath["ind"])+"\" target=\"_blank\"><img src=\""+str(imgp)+"\" loading=\"lazy\" class=\"imgborder\" alt=\""+str(imgpath["label"])+"\"/></a></div></div>"
                 imghtml+="<figcaption style=\"color:black\"><a href=\""+str(deploypath)+"/"+DocUtils.shortenURI(imgpath["ind"])+"\" style=\"font-weight:bold;color:black\" target=\"_blank\">"
                 if imgpath["label"]!="":
                    imghtml+=str(imgpath["label"])+"</a></figcaption></figure></li>"
