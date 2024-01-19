7173ff9793aae48184bb91fc47a94de0685a31dd
Timo <situx@users.noreply.github.com>
2024-01-19 16:17:11 +0100

    fixes

--- src/sparqlunicorn_ontdoc/docgeneration.py
+++ src/sparqlunicorn_ontdoc/docgeneration.py
@@ -546,7 +546,7 @@ class OntDocGeneration:
                 graph.add((URIRef("http://www.opengis.net/ont/geosparql#GeometryCollection"), URIRef("http://www.w3.org/2000/01/rdf-schema#subClassOf"),URIRef("http://www.opengis.net/ont/geosparql#SpatialObjectCollection")))
                 graph.add((URIRef(colluri), URIRef(self.typeproperty),URIRef("http://www.opengis.net/ont/geosparql#GeometryCollection")))
             elif cls in DocConfig.classToCollectionClass:
-                graph.add((URIRef("http://www.opengis.net/ont/geosparql#GeometryCollection"),
+                graph.add((URIRef(DocConfig.classToCollectionClass[cls]["class"]),
                            URIRef("http://www.w3.org/2000/01/rdf-schema#subClassOf"),
                            URIRef("http://www.w3.org/2004/02/skos/core#Collection")))
                 graph.add((URIRef(colluri),URIRef(self.typeproperty),URIRef(DocConfig.classToCollectionClass[cls]["class"])))
