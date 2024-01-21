071c58f30307142a93f46d5292a8a2c865e3a956
Timo <situx@users.noreply.github.com>
2024-01-21 16:20:24 +0100

    fixes

--- src/sparqlunicorn_ontdoc/export/data/voidexporter.py
+++ src/sparqlunicorn_ontdoc/export/data/voidexporter.py
@@ -55,7 +55,7 @@ class VoidExporter:
 
     @staticmethod
     def toHTML(stats):
-        result="<details>"
+        result="<details><summary>Dataset Statistics</summary>"
         result+="<table><thead><th>Property</th><th>Value</th></thead><tbody>"
         for stat in stats:
             result+="<tr><td><a href=\""+str(stat)+"\" target=\"_blank\">"+str(DocUtils.shortenURI(stat))+"</a></td><td>"+str(stats[stat])+"<a href=\"http://www.w3.org/2001/XMLSchema#integer\" style=\"color:#666;\">(xsd:integer)</a></td></tr>"
