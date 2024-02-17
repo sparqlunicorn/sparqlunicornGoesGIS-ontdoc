0570a9da3a572acdc2247c999ec660985563bc30
Timo <situx@users.noreply.github.com>
2024-02-17 18:34:32 +0100

    fixes

--- src/sparqlunicorn_ontdoc/doc/templateutils.py
+++ src/sparqlunicorn_ontdoc/doc/templateutils.py
@@ -68,6 +68,6 @@ class TemplateUtils:
                             templates[filename.replace(".html","")] = f.read()
         print("Found templates.... "+str(len(templates)))
         for temp in templates:
-            if temp!="includes" and temp!="layouts":
+            if temp!="includes" and temp!="layouts" and temp!="js":
                 templates[temp]=TemplateUtils.resolveIncludes(templates[temp],templates)
         return templates
