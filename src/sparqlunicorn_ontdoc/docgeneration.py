64852f018b19e78eb19b830006aa7224f6846614
Timo <situx@users.noreply.github.com>
2024-03-05 17:46:39 +0100

    fixes

--- src/sparqlunicorn_ontdoc/docgeneration.py
+++ src/sparqlunicorn_ontdoc/docgeneration.py
@@ -519,7 +519,7 @@ class OntDocGeneration:
         if "layouts" in templates:
             for template in templates["layouts"]:
                 if template!="main":
-                    self.getIncludesInTemplate(template)
+                    template=TemplateUtils.resolveIncludes(template,templates)
         else:
             sparqlhtml = self.replaceStandardVariables(templates["htmltemplate"], "", "0", "false")
             sparqlhtml = sparqlhtml.replace("{{iconprefixx}}",("icons/" if self.offlinecompat else "")).replace("{{baseurl}}", prefixnamespace).replace("{{relativedepth}}","0").replace("{{relativepath}}",".").replace("{{toptitle}}","SPARQL Query Editor").replace("{{title}}","SPARQL Query Editor").replace("{{startscriptpath}}", "startscripts.js").replace("{{stylepath}}", "style.css")\
