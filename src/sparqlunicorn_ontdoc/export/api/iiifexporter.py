31f5a938a0c8b73fb8f1b1dba747e5c0be20cd27
Timo <situx@users.noreply.github.com>
2024-04-04 16:16:42 +0200

    iiif openapi work

--- src/sparqlunicorn_ontdoc/export/api/iiifexporter.py
+++ src/sparqlunicorn_ontdoc/export/api/iiifexporter.py
@@ -179,6 +179,28 @@ class IIIFAPIExporter:
 
     @staticmethod
     def generateIIIFCollections(outpath, deploypath, imagespaths, prefixnamespace):
+        apihtml = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /><metaname=\"description\" content=\"SwaggerUI\"/><title>SwaggerUI</title><link rel=\"stylesheet\" href=\"https://unpkg.com/swagger-ui-dist/swagger-ui.css\" /></head><body><div id=\"swagger-ui\"></div><script src=\"https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js\" crossorigin></script><script>const swaggerUrl = \"" + str(deploypath) + "/iiif/api.json\"; const apiUrl = \"" + str(deploypath) + "/\";  window.onload = () => {let swaggerJson = fetch(swaggerUrl).then(r => r.json().then(j => {j.servers[0].url = apiUrl; window.ui = SwaggerUIBundle({spec: j,dom_id: '#swagger-ui'});}));};</script></body></html>"
+        apijson = {"openapi": "3.0.1", "info": {"title": str(deploypath) + " IIIF", "description": "IIIF API of " + str(deploypath)},"servers": [{"url": str(deploypath)}], "paths": {}}
+        apijson["paths"][outpath + "/iiif/collection/"] = {"get": {"tags": ["IIIF"],
+                                                                                "summary": "Retrieves IIIF Collections of "+str(deploypath),
+                                                                                "description": "Retrieves the IIIF Collections of this IIIF API",
+                                                                                "operationId": "iiif-collections",
+                                                                                "parameters": [],
+                                                                                "responses": {
+                                                                                    "200": {
+                                                                                        "description": "Success",
+                                                                                        "content": {
+                                                                                            "text/plain": {
+                                                                                                "example": None}},
+                                                                                        "application/json": {
+                                                                                            "schema": {
+                                                                                                "example": None},
+                                                                                            "example": None},
+                                                                                        "text/json": {
+                                                                                            "schema": {
+                                                                                                "example": None},
+                                                                                            "example": None}}}}}
+
         if not os.path.exists(outpath + "/iiif/collection/"):
             os.makedirs(outpath + "/iiif/collection/")
         if os.path.exists(outpath + "/iiif/collection/iiifcoll.json"):
@@ -227,4 +249,10 @@ class IIIFAPIExporter:
         iiifindex = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><script src="https://unpkg.com/mirador@latest/dist/mirador.min.js"></script></head><body><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"><div id="my-mirador"/><script type="text/javascript">var mirador = Mirador.viewer({"id": "my-mirador","manifests": {"collection/iiifcoll.json": {"provider": "Harvard University"}},"windows": [{"loadedManifest": "collection/iiifcoll.json","canvasIndex": 2,"thumbnailNavigationPosition": 'far-bottom'}]});</script></body></html>"""
         f = open(outpath + "/iiif/index.html", "w", encoding="utf-8")
         f.write(iiifindex)
+        f.close()
+        f = open(outpath + "/iiif/api.html", "w", encoding="utf-8")
+        f.write(apihtml)
+        f.close()
+        f = open(outpath + "/iiif/api.json", "w", encoding="utf-8")
+        f.write(apihtml)
         f.close()
