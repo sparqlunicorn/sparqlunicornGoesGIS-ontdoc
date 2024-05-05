96450b84800209a080d2358a66bab8166c9c8832
Timo <situx@users.noreply.github.com>
2024-05-05 23:49:09 +0200

    fixes

--- src/sparqlunicorn_ontdoc/resources/html/default/js/startscripts.js
+++ src/sparqlunicorn_ontdoc/resources/html/default/js/startscripts.js
@@ -1918,7 +1918,8 @@ function setupLeaflet(baselayers,epsg,baseMaps,overlayMaps,map,featurecolls,date
         clustersfrozen=!clustersfrozen
     }, 'toggleClusters')
     layercontrol=L.control.layers(baseMaps,overlayMaps).addTo(map)
-	if(featcounter>1 && dateatt!=null && dateatt!="" && dateatt!="[]" && dateatt!=[]){
+	createDropdownOptions(featurecolls)
+    if(featcounter>1 && dateatt!=null && dateatt!="" && dateatt!="[]" && dateatt!=[]){
         let textbox   = L.Control.extend({
             onAdd: function() {
                 var text = L.DomUtil.create('div');
