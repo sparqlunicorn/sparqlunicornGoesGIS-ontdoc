49c39f2976c75e5bac7285557b2be19e45a79489
Timo <situx@users.noreply.github.com>
2024-09-22 16:59:57 +0200

    Update solidexporter.py

--- src/sparqlunicorn_ontdoc/export/api/solidexporter.py
+++ src/sparqlunicorn_ontdoc/export/api/solidexporter.py
@@ -19,12 +19,12 @@ class SolidExporter:
         webidprofilegraph.add((URIRef(outpath+"/profile/card"), URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), URIRef("http://xmlns.com/foaf/0.1/PersonalProfileDocument")))
         preferencesgraph=Graph()
         preferencesgraph.serialize(destination=outpath+"/settings/prefs.ttl", format="ttl")
-        publisheruri=publisher
+        publisheruri=publisher.replace(" ","_")
         if not publisher.startswith("http"):
-            publisheruri=deploypath+"/profile/card#"+str(publisher)
+            publisheruri=deploypath+"/profile/card#"+str(publisher).replace(" ","_")
         webidprofilegraph.add((URIRef(outpath + "/profile/card"), URIRef("http://xmlns.com/foaf/0.1/primaryTopic"), URIRef(str(publisheruri))))
         webidprofilegraph.add((URIRef(str(publisheruri)), URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), URIRef("http://xmlns.com/foaf/0.1/Person")))
-        webidprofilegraph.add((URIRef(str(publisheruri)), URIRef("http://www.w3.org/ns/pim/space#storage"), URIRef(str(deploypath) + str(datasetname))))
+        webidprofilegraph.add((URIRef(str(publisheruri)), URIRef("http://www.w3.org/ns/pim/space#storage"), URIRef(str(deploypath) + str(datasetname).replace(" ","_"))))
         webidprofilegraph.add((URIRef(str(publisheruri)), URIRef("http://www.w3.org/ns/solid/terms#publicTypeIndex"), URIRef(deploypath + "/settings/publicTypeIndex.ttl")))
         webidprofilegraph.add((URIRef(str(publisheruri)), URIRef("http://www.w3.org/ns/solid/terms#privateTypeIndex"), URIRef(deploypath + "/settings/privateTypeIndex.ttl")))
         webidprofilegraph.serialize(destination=outpath+"/profile/card.ttl", format="ttl")
