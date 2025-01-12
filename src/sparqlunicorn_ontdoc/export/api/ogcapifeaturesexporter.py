a90dc7566c7e4428c1a627fe16badb0f8e48b615
Timo <situx@users.noreply.github.com>
2025-01-12 03:27:45 +0100

    fixes

--- src/sparqlunicorn_ontdoc/export/api/ogcapifeaturesexporter.py
+++ src/sparqlunicorn_ontdoc/export/api/ogcapifeaturesexporter.py
@@ -243,7 +243,7 @@ class OGCAPIFeaturesExporter:
                                 str(op + "/items/indexc.html").replace("//", "/"), outpath)
                             f = open(str(op + "/items/indexc.html"), "w")
                             f.write(
-                                "<html><head><meta http-equiv=\"refresh\" content=\"0; url=" + deploypath+"/"+featurecollectionspaths[coll]["name"]+"/"+ "\" /></head></html>")
+                                "<html><head><meta http-equiv=\"refresh\" content=\"0; url=" + deploypath+"/"+featurecollectionspaths[coll]["id"]+"/"+ "\" /></head></html>")
                             f.close()
                         #print("symlinks created")
                     except Exception as e:
