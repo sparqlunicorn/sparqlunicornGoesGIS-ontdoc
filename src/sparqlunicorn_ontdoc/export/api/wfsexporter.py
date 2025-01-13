00703fa43b4291c521a73a9a65ebbf36edd727cc
Timo <situx@users.noreply.github.com>
2025-01-13 17:29:22 +0100

    fixes

--- src/sparqlunicorn_ontdoc/export/api/wfsexporter.py
+++ src/sparqlunicorn_ontdoc/export/api/wfsexporter.py
@@ -6,9 +6,9 @@ class WFSExporter:
     @staticmethod
     def generateWFSPages10(outpath,deploypath,featurecollectionspaths,license):
         getcapabilities = f"""<?xml version="1.0" encoding="UTF-8" ?>
-        <wfs:WFS_Capabilities xmlns:wfs="http://www.opengis.net/wfs" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.0.0/WFS-capabilities.xsd https://www.baysis.bayern.de/gis/admin/services/BAYSIS_Strassennetz/MapServer/WFSServer https://www.baysis.bayern.de/gis/services/wfs/BAYSIS_Strassennetz/MapServer/WFSServer?service=wfs%26version=1.0.0%26request=DescribeFeatureType" version="1.0.0">
+        <wfs:WFS_Capabilities xmlns:wfs="http://www.opengis.net/wfs" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.0.0/WFS-capabilities.xsd" version="1.0.0">
             <wfs:Service>
-                <wfs:Name>Static WFS</wfs:Name>
+                <wfs:Name>Static WFS 1.0.0</wfs:Name>
                 <wfs:Title>Static WFS for {deploypath}</wfs:Title>
                 <wfs:Abstract>This Static WFS exposes geodata included in the knowledge grap for the inclusion into GIS applications.</wfs:Abstract>
                 <wfs:Keywords>{deploypath}</wfs:Keywords>
@@ -66,7 +66,98 @@ class WFSExporter:
         f.close()
 
     @staticmethod
-    def generateWFSPages(outpath,deploypath, featurecollectionspaths,license,wfsversion="1.0.0"):
+    def generateWFSPages11(outpath,deploypath,featurecollectionspaths,license):
+        getcapabilities="<wfs:WFS_Capabilities xmlns:ows=\"http://www.opengis.net/ows\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:wfs=\"http://www.opengis.net/wfs\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.3/wfs.xsd\" version=\"1.1.3\" updateSequence=\"0\">"
+        getcapabilities+=f"""
+        <ows:ServiceIdentification>
+            <ows:Title>Static WFS 1.1.0</ows:Title>
+            <ows:Abstract>This Static WFS exposes geodata included in the knowledge grap for the inclusion into GIS applications.</ows:Abstract>
+            <ows:Keywords>
+            <ows:Keyword>{deploypath}</ows:Keyword>
+            <ows:Type>String</ows:Type>
+            </ows:Keywords>
+            <ows:ServiceType>WFS</ows:ServiceType>
+            <ows:ServiceTypeVersion>1.1.3</ows:ServiceTypeVersion>
+            <ows:ServiceTypeVersion>1.1.0</ows:ServiceTypeVersion>
+            <ows:Fees>None</ows:Fees>
+            <ows:AccessConstraints>{license}</ows:AccessConstraints>
+        </ows:ServiceIdentification>
+        <wfs:FeatureTypeList>"""
+        for coll in featurecollectionspaths:
+            curcoll = None
+            if os.path.exists(coll):
+                with open(coll, 'r', encoding="utf-8") as infile:
+                    curcoll = json.load(infile)
+                curftype = f"""
+                <wfs:FeatureType>
+                    <wfs:Name>{str(coll.replace(outpath, "").replace("index.geojson", "").replace(".geojson", "")).rstrip("/")}</wfs:Name>
+                    <wfs:Title>{str(coll.replace(outpath, "").replace("index.geojson", "").replace(".geojson", "")).rstrip("/")}</wfs:Title>
+                    <wfs:Abstract>FeatureCollection {str(coll.replace(outpath, "").replace("index.geojson", "").replace(".geojson", "")).rstrip("/")}</wfs:Abstract>
+                    """
+            if "crs" in curcoll:
+                curftype += f"""<wfs:DefaultSRS>{curcoll["crs"]}</wfs:DefaultSRS>"""
+            else:
+                curftype += "<wfs:DefaultSRS>EPSG:4326</wfs:DefaultSRS>"
+            if "extent" in curcoll:
+                bboxcoords = curcoll["extent"]["spatial"]["bbox"][0]
+                curftype += f"""<ows:WGS84BoundingBox><ows:LowerCorner>{bboxcoords[0]}" {bboxcoords[1]}"</ows:LowerCorner><ows:UpperCorner>"{bboxcoords[2]}" "{bboxcoords[3]}"</ows:UpperCorner></ows:WGS84BoundingBox>"""
+            curftype += "</wfs:FeatureType>"
+            getcapabilities += curftype + "\n"
+        getcapabilities += f"""</wfs:FeatureTypeList>
+            <ogc:Filter_Capabilities></ogc:Filter_Capabilities>
+        </wfs:WFS_Capabilities>"""
+        print("SAVE WFS GETCAPABILITIES: " + str(outpath + "/wfs?request=GetCapabilities&service=WFS&version=1.0.0"))
+        f = open(outpath + "/wfs/index.xml", "w", encoding="utf-8")
+        f.write(getcapabilities)
+        f.close()
+
+    @staticmethod
+    def generateWFSPages20(outpath,deploypath,featurecollectionspaths,license):
+        getcapabilities="<WFS_Capabilities xmlns=\"http://www.opengis.net/wfs/2.0\" xmlns:gml=\"http://www.opengis.net/gml/3.2\" xmlns:fes=\"http://www.opengis.net/fes/2.0\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:ows=\"http://www.opengis.net/ows/1.1\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"2.0.2\" xsi:schemaLocation=\"http://www.opengis.net/wfs/2.0 http://schemas.opengis.net/wfs/2.0/wfs.xsd http://www.opengis.net/ows/1.1 http://schemas.opengis.net/ows/1.1.0/owsAll.xsd\">"
+        getcapabilities+=f"""
+        <ows:ServiceIdentification>
+            <ows:Title>Static WFS 1.1.0</ows:Title>
+            <ows:Abstract>This Static WFS exposes geodata included in the knowledge grap for the inclusion into GIS applications.</ows:Abstract>
+            <ows:Keywords>
+            <ows:Keyword>{deploypath}</ows:Keyword>
+            <ows:Type>String</ows:Type>
+            </ows:Keywords>
+            <ows:ServiceType>WFS</ows:ServiceType>
+            <ows:ServiceTypeVersion>1.1.3</ows:ServiceTypeVersion>
+            <ows:ServiceTypeVersion>1.1.0</ows:ServiceTypeVersion>
+            <ows:Fees>None</ows:Fees>
+            <ows:AccessConstraints>{license}</ows:AccessConstraints>
+        </ows:ServiceIdentification>
+        <wfs:FeatureTypeList>"""
+        for coll in featurecollectionspaths:
+            curcoll = None
+            if os.path.exists(coll):
+                with open(coll, 'r', encoding="utf-8") as infile:
+                    curcoll = json.load(infile)
+                curftype = f"""
+                            <wfs:FeatureType>
+                    <wfs:Name>{str(coll.replace(outpath, "").replace("index.geojson", "").replace(".geojson", "")).rstrip("/")}</wfs:Name>
+                    <wfs:Title>{str(coll.replace(outpath, "").replace("index.geojson", "").replace(".geojson", "")).rstrip("/")}</wfs:Title>
+                    <wfs:Abstract>FeatureCollection {str(coll.replace(outpath, "").replace("index.geojson", "").replace(".geojson", "")).rstrip("/")}</wfs:Abstract>
+                    """
+            if "crs" in curcoll:
+                curftype += f"""<wfs:SRS>{curcoll["crs"]}</wfs:SRS>"""
+            else:
+                curftype += "<wfs:SRS>EPSG:4326</wfs:SRS>"
+            if "extent" in curcoll:
+                bboxcoords = curcoll["extent"]["spatial"]["bbox"][0]
+                curftype += f"""<wfs:LatLongBoundingBox minx="{bboxcoords[0]}" miny="{bboxcoords[1]}" maxx="{bboxcoords[2]}" maxy="{bboxcoords[3]}"/>"""
+            curftype += "</wfs:FeatureType>"
+            getcapabilities += curftype + "\n"
+        getcapabilities += f"""</wfs:FeatureTypeList>
+            <ogc:Filter_Capabilities></ogc:Filter_Capabilities>
+        </wfs:WFS_Capabilities>"""
+        f = open(outpath + "/wfs/index.xml", "w", encoding="utf-8")
+        f.write(getcapabilities)
+        f.close()
+
+    @staticmethod
+    def generateWFSPages(outpath,deploypath, featurecollectionspaths,license,wfsversion="1.1.0"):
         os.mkdir(outpath+"/wfs")
         apihtml = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /><metaname=\"description\" content=\"SwaggerUI\"/><title>SwaggerUI</title><link rel=\"stylesheet\" href=\"https://unpkg.com/swagger-ui-dist/swagger-ui.css\" /></head><body><div id=\"swagger-ui\"></div><script src=\"https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js\" crossorigin></script><script>const swaggerUrl = \"" + str(
             deploypath) + "/api/index.json\"; const apiUrl = \"" + str(deploypath) + "/\";  window.onload = () => {let swaggerJson = fetch(swaggerUrl).then(r => r.json().then(j => {j.servers[0].url = apiUrl; window.ui = SwaggerUIBundle({spec: j,dom_id: '#swagger-ui'});}));};</script></body></html>"
@@ -75,3 +166,7 @@ class WFSExporter:
                    "servers": [{"url": str(deploypath)}], "paths": {}}
         if wfsversion=="1.0.0":
             WFSExporter.generateWFSPages10(outpath,deploypath,featurecollectionspaths,license)
+        elif wfsversion.startswith("1.1"):
+            WFSExporter.generateWFSPages11(outpath, deploypath, featurecollectionspaths, license)
+        elif wfsversion=="2" or wfsversion.startswith("2."):
+            WFSExporter.generateWFSPages20(outpath, deploypath, featurecollectionspaths, license)
