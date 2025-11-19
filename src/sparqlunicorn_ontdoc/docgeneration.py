b0701261bf700654407b5ced092456e43d04229a
Timo Homburg <situx@users.noreply.github.com>
2025-11-19 14:29:13 +0100

    Update docgeneration.py

--- src/sparqlunicorn_ontdoc/docgeneration.py
+++ src/sparqlunicorn_ontdoc/docgeneration.py
@@ -558,7 +558,8 @@ def main():
                 curpath=outpath[-1]
             subrend = docgen.generateOntDocForNameSpace(args.prefixns, dataformat="HTML")
             DocUtils.printExecutionStats(docgen.exectimes)
-            DocUtils.writeExecutionStats(docgen.exectimes,curpath+"/buildlog.csv")
+            DocUtils.writeExecutionStats(docgen.exectimes,curpath+"/buildlog.json")
+            DocUtils.writeExecutionStats(ontdoc.exectimes, curpath+"/buildlog.js")
         except Exception as inst:
             print("Could not parse " + str(fp))
             print(inst)
