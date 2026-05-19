f3c4ef077b212b70387341276f505818dfe42f30
Timo Homburg <situx@users.noreply.github.com>
2026-05-19 12:29:32 +0200

    Update owltimepage.py

--- src/sparqlunicorn_ontdoc/export/pages/owltimepage.py
+++ src/sparqlunicorn_ontdoc/export/pages/owltimepage.py
@@ -35,6 +35,7 @@ class OWLTimePage:
     @staticmethod
     def timeObjectToHTML(timeobj,prefixes):
         timeres = ""
+        print("TOBJ to HTML"+str(timeobj))
         if "begin" in timeobj:
             if "end" in timeobj:
                 if str(timeobj["begin"]) in DocConfig.timecalendarinstances:
